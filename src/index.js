eef31687375159a6edf7bd389761a90d77e9279d
Jenna Lee <65867683+roasted-oolong@users.noreply.github.com>
2025-08-21 15:36:36 +0000

    Add tab-switching logic
    Added tab-switching logic. When a button is pressed, an event listener calls the correct module for each tab.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,25 @@ import { loadMenu } from "./menu.js";
 import { loadAbout } from "./about.js";
 
 console.log(greeting)
+
+const content = document.getElementById('content');
+const navs = document.querySelectorAll('header nav');
+
+if (navs.length >= 3) {
+  navs[0].addEventListener('click', () => {
+    content.innerHTML = '';
+    loadHomepage();
+  });
+  navs[1].addEventListener('click', () => {
+    content.innerHTML = '';
+    loadMenu();
+  });
+  navs[2].addEventListener('click', () => {
+    content.innerHTML = '';
+    loadAbout();
+  });
+}
+
+// Load default tab
+content.innerHTML = '';
 loadHomepage();
-loadMenu();
-loadAbout();
